188de53cfe2a485e409a739f1438ceb5a264dd70
florind <anotherflo@yahoo.com>
2011-09-05 22:07:49 +0200

    Web service tests

---
+++ assets/spec/serviceTest.js
@@ -0,0 +1,47 @@
+var http = require('http'),
+  server = require('../controller/rootDispatcher.js').server;
+
+var serverPort = 40101;
+server.listen(serverPort);
+
+var client = http.createClient(serverPort, 'localhost');
+
+exports['GET nonexistent file should return 404 Not Found'] =function(test) {
+  var req = client.request('GET', '/nothing');
+  req.on('response', function(res) {
+    res.on('end', function() {
+      test.equal(res.statusCode, 404);
+
+      cleanup();
+      test.done();
+    });
+  });
+  req.end();
+};
+
+exports['GET /health should return OK'] =function(test) {
+  var req = client.request('GET', '/health');
+  req.on('response', function(res) {
+    res.on('end', function() {
+      test.equal(res.statusCode, 200);
+
+      cleanup();
+      test.done();
+    });
+  });
+  req.end();
+};
+
+var globalTestCount = 2;  //total number of tests.
+var currentExecutedTestCount = 0;
+/*
+This method increments the number of executed tests
+Call cleanup() after test.done.
+shutdown the server if all tests have executed.
+*/
+function cleanup() {
+  currentExecutedTestCount++;
+  if(currentExecutedTestCount == globalTestCount) {
+    server.close();
+  }
+}
